1749c4fd71f4e78ea45fb9f302de2a6de059ac50
MAHafizh <hafizh12jr@gmail.com>
2026-02-08 01:03:25 +0700

    edit DB name

--- server/src/lib/database.js
+++ server/src/lib/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Connected to MongoDB ${conn.connection.host}`);
+    console.log(`Connected to MongoDB ${conn.connection.name}`);
   } catch (error) {
     console.log("Failed Connected to MongoDB", error);
     process.exit(1);
